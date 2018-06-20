fefcd300f91ab93f6703b455f82d33b6cfa7c82d
cafe-serendipity <cafe.serendipity21@gmail.com>
2018-06-20 22:15:08 +0200

    reset

---
+++ src/components/VueMagicLineTab.vue
@@ -0,0 +1,39 @@
+<template> 
+  <transition name="fade">
+    <div 
+    	class="magic-line-item-content" 
+      	v-show="isActive"  
+         :key="Math.floor(Math.random()*10000)"
+        :class="{ active: isActive, disabled: isDisabled }"> 
+        <slot/>  
+    </div> 
+  </transition>
+</template>
+
+<script> 
+    export default {
+      name: 'vue-magic-line-tab',
+      props: {
+        name: {
+          type: String,
+          required: true
+        }
+      },
+      data () {
+        return {
+        	show: false,
+          isActive: false,
+          isDisabled: false,
+        }
+      }
+    }
+</script>
+
+<style lang="scss">  
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
